66f5211c7ce0496de0d634b514db8864ed898385
yucongxing <yucongxing@163.com>
2022-06-07 20:11:04 +0800

    add F5Compile function and <leader>rc keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -13,3 +13,22 @@ keymap('v', '<', '<gv', opts)
 keymap('n', '<leader>l', ':nohl<CR>', opts)
 
 keymap('n', '<leader>c', ':cclose<CR>', opts)
+
+_G.F5Compile = function()
+  vim.cmd(':w')
+  vim.opt.splitbelow = true
+  vim.cmd(':sp')
+  vim.cmd(':res 10')
+  if vim.bo.filetype == 'cpp' then
+    vim.cmd(':term g++ -std=c++17 -Wall % -o %<.out && %<.out')
+  elseif vim.bo.filetype == 'c' then
+    vim.cmd(':term gcc -Wall % -o %<.out && %<.out')
+  elseif vim.bo.filetype == 'py' then
+    vim.cmd(':term python %')
+  elseif vim.bo.filetype == 'sh' then
+    vim.cmd(':term bash %')
+  end
+end
+
+keymap('n', '<F5>', '<cmd>lua F5Compile()<CR>', opts)
+keymap('n', '<leader>rc', ':vsplit $MYVIMRC<CR>', opts)

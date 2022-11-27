7d9e906ad0a188ff3e429a8a9c8d71cd030c0e4a
yucongxing <yucongxing@163.com>
2022-11-27 15:11:47 +0800

    remove some deprecated api

--- lua/user/lsp/lsp_config.lua
+++ lua/user/lsp/lsp_config.lua
@@ -77,6 +77,6 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
